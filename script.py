0eee37ae61b4870bff23c29c8086e1930d6c2d17
oldmcdonnell <oldmcdonnell@gmail.com>
2024-05-09 13:40:27 -0400

    winning comdition

--- script.py
+++ script.py
@@ -7,9 +7,9 @@ import itertools
 
 suits = ['♥', '♦', '♣', '♠']
 
-values = ['ace', '2', '3', '4', '5', '6', 
+values = ['2', '3', '4', '5', '6', 
           '7', '8', '9', '10', 
-          'Jack', 'Queen', 'King']
+          'Jack', 'Queen', 'King', 'Ace']
 
 #basic card class
 class Card:
@@ -29,6 +29,17 @@ class Deck():
 
     def __str__(self):
         return "\n".join(str(card) for card in self.cards)
+    
+class Winner():
+    def __init__(self, packs = 1):
+        self.packs = packs
+        self.cards = [Card(value, suit) for value, suit in itertools.product(values, suits) 
+                      for _pack in range(packs)]
+
+
+    def __str__(self):
+        return "\n".join(str(card) for card in self.cards)
+
 
 class Player():
     def __init__(self, name, hand):
@@ -42,7 +53,18 @@ class Player():
 
 # print(deck_test.cards[0], 'and' ,deck_test.cards[1])
 
-    
+test1 = Winner()
+test2 = Winner()
+if str(test1.cards[1]) == str(test2.cards[1]):
+    print("test success")
+else:
+    print("test fail")
+    print(test1.cards[1], test2.cards[1])
+
+for i in test1.cards:
+    print(i)
+
+
 def introduce_player():
     name_input = input("Hello, please input your name: ")
     deck_input = input("how many decks do you want to play with? ")
@@ -54,13 +76,18 @@ def introduce_player():
     print('Hello', player_one.name)
     print("Your card is the", player_one.hand)
     print("The computer cards is the", computer_player.hand)
-    return player_one.hand, computer_hand 
+    return player_one, computer_player 
 
 
 hands = introduce_player()
+# winner_list = [Card(value, suit) for value in values for suit in suits]
 
 def determine_winner(hands):
-    
-    time.sleep(3)
-    print(hands[0], hands[1])
+    winner_list = Winner()
+    # if winner_list.cards.index(hands[0]) > winner_list.cards.index(hands[1]):
+    #     print("player wins")
+    # else:
+    #     print("computer wins")
+    # time.sleep(3)
+    print(f'{hands[0].name}s hand is the {hands[0].hand}, Computer hand is the {hands[1].hand}')
 determine_winner(hands)
