daa5ddc764302f059785ac387286bc1afead7faa
oldmcdonnell <oldmcdonnell@gmail.com>
2024-05-09 09:33:46 -0400

    pushing random code

--- script.py
+++ script.py
@@ -1,11 +1,11 @@
+import copy
 import sys
 import time
 import random
+import itertools
 
-print("program activated")
 
-
-colors = ['♥', '♦', '♣', '♠']
+suits = ['♥', '♦', '♣', '♠']
 
 values = ['ace', '2', '3', '4', '5', '6', 
           '7', '8', '9', '10', 
@@ -13,35 +13,58 @@ values = ['ace', '2', '3', '4', '5', '6',
 
 #basic card class
 class Card:
-    def __init__(self, value, color):
+    def __init__(self, value, suit):
         self.value = value
-        self.color = color
+        self.suit = suit
+    
+    def __str__(self):
+        return f"{self.value} of {self.suit}"
 
 class Deck():
-    def __init__(self, amount = 1):
-        self.amount = amount
-        shuffled = random.shuffle([Card(value, color) for value in values for color in colors])
-        self.cards = shuffled
+    def __init__(self, packs = 1):
+        self.packs = packs
+        self.cards = [Card(value, suit) for value, suit in itertools.product(values, suits) 
+                      for _pack in range(packs)]
+        random.shuffle(self.cards)
+        # shuffled = random.shuffle([Card(value, suit) for value in values for suit in suits])
+        # self.cards = shuffled
+
+    def __str__(self):
+        return "\n".join(str(card) for card in self.cards)
+
+
+if __name__ == "__main__":
+    deck = Deck(packs=2)
+    print(deck)      
+
+
 
 # test_deck = Deck()
 # for i in test_deck.cards:
 #     print(i.color, i.value)
 
 
-def multi_deck_fn():
-    num = input("how many decks?" )
-    num_decks = Deck()
-    new_list = []
-    
-    new_list.extend([num_decks] * num)
-    return new_list
+# def multi_deck_fn():
+#     num = input("how many decks?" )
+#     num_decks = Deck()
+#     new_list = []
 
+#     new_list.extend([num_decks] * num)
+#     return new_list
 
-multi_deck = multi_deck_fn()
-print(multi_deck)
+# deck1 = Deck()
+# deck2 = Deck()
 
-for i in multi_deck.cards:
-    print('multi', i.color, i.value)
+# multi_deck = deck1 + deck2
+
+# for i in multi_deck.cards:
+#     print('multi', i.color, i.value)
+
+# # multi_deck = multi_deck_fn()
+# print(multi_deck)
+
+# for i in multi_deck.cards:
+#     print('multi', i.color, i.value)
 
 # print(test_deck.order)
 
@@ -53,9 +76,9 @@ for i in multi_deck.cards:
 #         print(multi_deck.value, multi_deck.color)
 #         #multi_deck = #number of decs
 #         #shuffle(multideck)
-print("♥♦♣♠")
+# print("♥♦♣♠")
 
-print ('\u2663')
+# print ('\u2663')
 
 # deck = [Card(value, color) for value in values for color in colors]
 # random.shuffle(deck)
